083efbc9db6e9f612c478e121b511e0eb6298a18
Ell Neal <elliot@emdentec.com>
2016-01-10 22:06:46 +0000

    Make docker available to jenkins after first build

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,6 @@ sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list
 sudo apt-get -y update
 sudo apt-get -y install jenkins
 
-# start jenkins
-/etc/init.d/jenkins start
-
 # docker setup
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sh -c 'echo deb https://apt.dockerproject.org/repo debian-jessie main > /etc/apt/sources.list.d/docker.list'
@@ -20,5 +17,5 @@ apt-get install -y docker-engine
 usermod -a -G docker vagrant
 usermod -a -G docker jenkins
 
-# start docker
-/etc/init.d/docker start
+# start jenkins
+/etc/init.d/jenkins restart
